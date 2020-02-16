0f1f418b3df9ad6f0494efbc4bb19fc929952f0e
Leo <zhixiangleo@163.com>
2020-02-16 15:41:19 +0800

    编码格式修改
    编码格式转为utf-8，中文正常显示

--- simscape/test2_num.m
+++ simscape/test2_num.m
@@ -1,9 +1,9 @@
 clear
 clc
-% ÏÈ¹«Ê½ÍÆµ¼µÃµ½½âÎö½â
-% ÔÙ¸ù¾İ¸÷¹Ø½ÚÎ»ÖÃ¡¢ËÙ¶È¡¢¼ÓËÙ¶ÈÒÔ¼°Á¦¾Ø·´À¡Öµ±æÊ¶µÃµ½¹ßĞÔ²ÎÊı
+% å…ˆå…¬å¼æ¨å¯¼å¾—åˆ°è§£æè§£
+% å†æ ¹æ®å„å…³èŠ‚ä½ç½®ã€é€Ÿåº¦ã€åŠ é€Ÿåº¦ä»¥åŠåŠ›çŸ©åé¦ˆå€¼è¾¨è¯†å¾—åˆ°æƒ¯æ€§å‚æ•°
 
-% Õı³£Á¬¸Ë£¬¾ßÓĞÖÊÁ¿ºÍ¹ßÁ¿£¬ÖÊĞÄÎ»ÖÃÓë¼¸ºÎÖĞĞÄÖØºÏ£¨Á¢·½ÌåÁ¬¸Ë£©
+% æ­£å¸¸è¿æ†ï¼Œå…·æœ‰è´¨é‡å’Œæƒ¯é‡ï¼Œè´¨å¿ƒä½ç½®ä¸å‡ ä½•ä¸­å¿ƒé‡åˆï¼ˆç«‹æ–¹ä½“è¿æ†ï¼‰
 syms m1 m2 len1 len2 g I1 I2
 syms q1 q2 dq1 dq2 ddq1 ddq2 
 syms v1 v2 w1 w2
@@ -18,15 +18,15 @@ vc1 = v1+w1*len1/2;
 vc2_vec = [-sin(x1)*len1*dx1-len2/2*sin(x1+x2)*(dx1+dx2); ...
                   cos(x1)*len1*dx1+len2/2*cos(x1+x2)*(dx1+dx2)];
 vc2 = norm(vc2_vec);
-%¶¯ÄÜ
+%åŠ¨èƒ½
 T1 = 1/2*m1*vc1^2+1/2*I1*w1^2;
 T2 = 1/2*m2*vc2^2+1/2*I2*w2^2;
 T = T1+T2;
-%ÖØÁ¦ÊÆÄÜ
+%é‡åŠ›åŠ¿èƒ½
 U1 = 1/2*m1*g*len1*sin(x1);
 U2 = m2*g*(len1*sin(x1)+len2/2*sin(x2+x1));
 U = U1 + U2;
-%À­¸ñÀÊÈÕ·½³Ì
+%æ‹‰æ ¼æœ—æ—¥æ–¹ç¨‹
 E = T - U;
 E = subs(E,{x1,dx1,ddx1,x2,dx2,ddx2},{q1,dq1,ddq1,q2,dq2,ddq2});
 dEddq1 = diff(E,dq1);
@@ -43,8 +43,8 @@ dEdq1 = diff(E,q1);
 dEdq2 = diff(E,q2);
 tau1 = dEddq1dt - dEdq1;
 tau2 = dEddq2dt - dEdq2;
-%% »¯Îª¾ØÕóĞÎÊ½
-%%coeffs(a*x^3+2*x+1,x,'All')£¬ÄÜ¹»ÌáÈ¡³öÎªÁãµÄÏµÊı
+%% åŒ–ä¸ºçŸ©é˜µå½¢å¼
+%%coeffs(a*x^3+2*x+1,x,'All')ï¼Œèƒ½å¤Ÿæå–å‡ºä¸ºé›¶çš„ç³»æ•°
 tau1_m1 = diff(tau1,m1);
 tau1_m2 = diff(tau1,m2);
 tau1_I1 = diff(tau1,I1);
@@ -56,7 +56,7 @@ tau2_I2 = diff(tau2,I2);
 Y = [tau1_m1, tau1_m2, tau1_I1, tau1_I2;
         tau2_m1, tau2_m2, tau2_I1, tau2_I2];
 Y = simplify(Y);
-%% ÊıÖµÇó½â£¬±æÊ¶¹ßĞÔ²ÎÊı
+%% æ•°å€¼æ±‚è§£ï¼Œè¾¨è¯†æƒ¯æ€§å‚æ•°
 %{
 g = 9.8 ;len1= 0.5; len2 = 0.5;
 Y_num = [];
@@ -90,7 +90,7 @@ for i=1:length(tout)
 end
 p = inv(Y_num(3:end,:)'*Y_num(3:end,:))*Y_num(3:end,:)'*tau_num(3:end)
 %}
-%% ¸ù¾İY¾ØÕó±í´ïÊ½£¬ÀûÓÃ×îĞ¡¶ş³Ë·¨Çó½âµÃµ½¹ßĞÔ²ÎÊı
+%% æ ¹æ®YçŸ©é˜µè¡¨è¾¾å¼ï¼Œåˆ©ç”¨æœ€å°äºŒä¹˜æ³•æ±‚è§£å¾—åˆ°æƒ¯æ€§å‚æ•°
 Y_num = [];
 tau_num = [];
 g = 9.8; len1 = 0.5; len2 = 0.5;
@@ -101,12 +101,12 @@ for i=1:length(tout)
     dq2 = dq2_num(i);
     ddq1 = ddq1_num(i);
     ddq2 = ddq2_num(i);
-    %evalº¯Êı½«×Ô±äÁ¿ÓÃÊıÖµ´úÌæ£¬²¢ÇÒ½«½á¹ûÓÉsym->double
+    %evalå‡½æ•°å°†è‡ªå˜é‡ç”¨æ•°å€¼ä»£æ›¿ï¼Œå¹¶ä¸”å°†ç»“æœç”±sym->double
     tmpY = eval(Y);
     Y_num = [Y_num;tmpY];
     
     tmp_tau = [tau1_num(i);tau2_num(i)];
     tau_num = [tau_num;tmp_tau];        
 end
-%¹ßĞÔ²ÎÊı
+%æƒ¯æ€§å‚æ•°
 p = inv(Y_num(3:end,:)'*Y_num(3:end,:))*Y_num(3:end,:)'*tau_num(3:end)
